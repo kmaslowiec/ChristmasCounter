9c2dde7e2925e60656af8b29428850d5f53f73bd
Kondzik <kmaslowiec@gmail.com>
2017-11-23 19:05:35 +0100

    Version 0.5
    Includes simple counter

---
+++ app/src/main/java/com/example/android/countdonwapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.countdonwapp;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        time = (TextView) findViewById(R.id.text);
+
+        time.setText("00:03:00");
+
+        final CounterClass time = new CounterClass(25000, 1000);
+        time.start();
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    @SuppressLint("NewApi")
+    public class CounterClass extends CountDownTimer {
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @SuppressLint("NewApi")
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MICROSECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis)
+                            - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            System.out.println(hms);
+            time.setText(hms);
+        }
+
+        @Override
+        public void onFinish() {
+
+            time.setText("God help");
+
+        }
+    }
+}
